a52569fc23a1d735e8959ec8e6cb799536a6674d
“Zhu Chen” <znchen720@gmail.com>
2017-05-22 16:07:28 -0400

    Add "updated_at" attribute to patient serializer

--- app/serializers/patient_serializer.rb
+++ app/serializers/patient_serializer.rb
@@ -1,3 +1,3 @@
 class PatientSerializer < ActiveModel::Serializer
-  attributes :id, :primary_doctor, :first_name, :last_name, :phone, :height, :weight, :age
+  attributes :id, :primary_doctor, :first_name, :last_name, :phone, :height, :weight, :age, :updated_at
 end
